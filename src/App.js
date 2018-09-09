5a882a20464ee12be63482fae1116bb270d00d3f
Jason Laster <jason.laster.11@gmail.com>
2018-09-09 12:26:13 -0400

    add stars + prettier

--- src/App.js
+++ src/App.js
@@ -1,77 +1,112 @@
-import React, { Component } from 'react';
-import { candidates } from "./bios"
-import Bio from './Bio'
+import React, { Component } from "react";
+import { candidates } from "./bios";
+import Bio from "./Bio";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import "./App.css";
 
-import './App.css';
+const candidateList = Object.values(candidates);
 
-const candidateList = Object.values(candidates)
-
-function Candidate({candidate, selected, search}) {
-  const isSelected = candidate.id === selected ? "selected" : ""
+function Candidate({ candidate, selected, starred, toggleStar, search }) {
+  const id = candidate.id;
+  const isSelected = id === selected ? "selected" : "";
   return (
-    <Link className={`candidate ${isSelected}`} to={`/${candidate.id}`}>
-      <img className="candidate-img" src={candidate.img}></img>
+    <Link className={`candidate ${isSelected}`} to={`/${id}`}>
+      <img className="candidate-img" src={candidate.img} />
       <div className="candidate-summary">
         <div className="top">{candidate.name}</div>
         <div className="location">{candidate.location}</div>
-        <div className="keywords">{candidate.keywords.map(key =>
-          <span key={key} onClick={() => search(key)} className="keyword">{key}</span>)}
+        <div className="keywords">
+          {candidate.keywords.map(key => (
+            <span key={key} onClick={() => search(key)} className="keyword">
+              {key}
+            </span>
+          ))}
         </div>
       </div>
+      {
+        <div
+          className={`candidate-star ${starred ? "star-selected" : ""}`}
+          onClick={e => {
+            e.stopPropagation();
+            e.preventDefault();
+            toggleStar(id);
+          }}
+        />
+      }
     </Link>
   );
 }
 
 function matchCandidate(query, candidate) {
   const rQuery = new RegExp(query, "i");
-  return query === "" ||
+  return (
+    query === "" ||
     candidate.name.match(rQuery) ||
     candidate.location.match(rQuery) ||
     candidate.keywords.join(" ").match(rQuery)
+  );
 }
 
-function CandidateList({candidates, selected, search}) {
-  return  <div className="candidates">
-      {candidates.map(candidate => <Candidate key={candidate.id} search={search} candidate={candidate} selected={selected} />)}
-    </div>
+function starCount(stars) {
+  return Object.values(stars).reduce(
+    (count, star) => count + (star ? 1 : 0),
+    0
+  );
 }
 
 function scrollToSelectedEl() {
-  const selectedEl = document.querySelector(".selected")
+  const selectedEl = document.querySelector(".selected");
   const top = selectedEl.getBoundingClientRect().top;
   if (top < 0 || top > window.outerHeight) {
-    selectedEl.scrollIntoView({block: "center", behavior: "instant"})
+    selectedEl.scrollIntoView({ block: "center", behavior: "instant" });
   }
 }
 
 class App extends Component {
   selected = null;
-  state = {query: ""};
+  state = {
+    query: "",
+    stars: JSON.parse(localStorage.getItem("stars")) || {},
+    showStars: false
+  };
 
   onSearch(e) {
-    const {value} = e.target;
-    this.setState({query: value})
+    const { value } = e.target;
+    this.setState({ query: value });
   }
+
+  toggleStar = id => {
+    let { stars } = this.state;
+
+    const star = stars[id] ? !stars[id] : true;
+    stars = { ...stars, [id]: star };
+
+    this.setState({ stars });
+    localStorage.setItem("stars", JSON.stringify(stars));
+  };
 
   search = query => {
-    this.setState({query})
-  }
+    this.setState({ query });
+  };
+
+  toggleShowStars = () => {
+    this.setState({ showStars: !this.state.showStars });
+  };
 
   componentDidMount() {
-    scrollToSelectedEl()
+    scrollToSelectedEl();
   }
 
-  CandidatesRoute = ({match}) =>{
-    const  { candidateId } = match.params
-    const {query} = this.state;
-    const filteredList = candidateList.filter(candidate => matchCandidate(query, candidate))
+  CandidatesRoute = ({ match }) => {
+    const { candidateId } = match.params;
+    const { query, stars, showStars } = this.state;
+    const filteredList = candidateList.filter(
+      candidate =>
+        matchCandidate(query, candidate) &&
+        (showStars ? stars[candidate.id] : true)
+    );
 
     let selected = filteredList.length > 0 ? filteredList[0].id : null;
     if (candidateId && filteredList.map(c => c.id).includes(candidateId)) {
@@ -81,31 +116,53 @@ class App extends Component {
     this.selected = selected;
     return (
       <div className="App">
-        <div  className="sidebar">
+        <div className="sidebar">
           <div className="searchbar">
-            <input value={query} className="search" placeholder="Search..." type="text" onChange={e => this.onSearch(e)} />
+            <input
+              value={query}
+              className="search"
+              placeholder="Search..."
+              type="text"
+              onChange={e => this.onSearch(e)}
+            />
           </div>
-          {filteredList.length > 0 ?
-             <CandidateList selected={selected} candidates={filteredList} search={this.search} />
-             : <div className="no-matches">No Matches...</div>}
-
+          {filteredList.length > 0 ? (
+            <div className="candidates">
+              {filteredList.map(candidate => (
+                <Candidate
+                  key={candidate.id}
+                  search={this.search}
+                  starred={stars[candidate.id]}
+                  toggleStar={this.toggleStar}
+                  candidate={candidate}
+                  selected={selected}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="no-matches">No Matches...</div>
+          )}
         </div>
 
-          <Bio selected={selected} />
-
-      </div>)
-  }
+        <Bio
+          selected={selected}
+          starCount={starCount(stars)}
+          toggleShowStars={this.toggleShowStars}
+          showStars={showStars}
+        />
+      </div>
+    );
+  };
 
   render() {
-    const {query} = this.state
+    const { query } = this.state;
     return (
       <Router>
         <div className="app-wrapper">
-          <Route path="/:candidateId" component={this.CandidatesRoute}/>
-          <Route exact path="/" component={this.CandidatesRoute}/>
+          <Route path="/:candidateId" component={this.CandidatesRoute} />
+          <Route exact path="/" component={this.CandidatesRoute} />
         </div>
       </Router>
-
     );
   }
 }
--- src/Bio.js
+++ src/Bio.js
@@ -1,56 +1,95 @@
-import React from 'react';
-import { CandidateBio } from "./bios"
-import { candidates } from "./bios"
+import React from "react";
+import { CandidateBio } from "./bios";
+import { candidates } from "./bios";
 
-import "./Bio.css"
+import "./Bio.css";
 
-function Header({candidate}) {
+function Header({ candidate }) {
+  const youtube =
+    candidate.youtube ||
+    `https://www.youtube.com/results?search_query=${candidate.name.replace(
+      / /,
+      "+"
+    )}`;
+  return (
+    <div className="candidate-header">
+      <img alt={candidate.name} className="candidate-img" src={candidate.img} />
+      <div className="candidate-summary">
+        <div className="top">{candidate.name}</div>
+        <div className="bottom">{candidate.location}</div>
 
-  const youtube = candidate.youtube || `https://www.youtube.com/results?search_query=${candidate.name.replace(/ /, '+')}`
-
-  return <div className="candidate-header">
-    <img alt={candidate.name} className="candidate-img" src={candidate.img}></img>
-    <div className="candidate-summary">
-      <div className="top">{candidate.name}</div>
-      <div className="bottom">{candidate.location}</div>
-
-      <div className="candidate-links">
-        <a className="button" href={candidate.donate}>Donate</a>
-        <a className="icon facebook" href={candidate.facebook}></a>
-        <a className="icon twitter" href={candidate.twitter}></a>
-        <a className="icon youtube" href={youtube}></a>
-        <a className="icon emily" href={candidate.link}></a>
-      </div>
-    </div>
-  </div>
-}
-
-function Article({candidate}) {
-   return <article><CandidateBio id={candidate.id} /></article>
-}
-
-export default function renderCandidateBio({selected}) {
-  const candidate = candidates[selected];
-  if (!candidate) {
-    return <div className="candidate-bio">
-      <div className="header">
-        <div class="links">
-          <a href="https://github.com/jasonlaster/emily">Help</a>
+        <div className="candidate-links">
+          <a className="button" href={candidate.donate}>
+            Donate
+          </a>
+          <a className="icon facebook" href={candidate.facebook} />
+          <a className="icon twitter" href={candidate.twitter} />
+          <a className="icon youtube" href={youtube} />
+          <a className="icon emily" href={candidate.link} />
         </div>
       </div>
     </div>
-  }
+  );
+}
 
-  return <div key={candidate.id} className="candidate-bio">
+function Article({ candidate }) {
+  return (
+    <article>
+      <CandidateBio id={candidate.id} />
+    </article>
+  );
+}
+
+function Banner({ starCount, toggleShowStars, showStars }) {
+  const noStars = starCount == 0;
+  return (
     <div className="header">
-
+      <div
+        className={`stars ${noStars ? "no-stars" : ""} ${
+          showStars ? "active" : ""
+        }`}
+        onClick={toggleShowStars}
+      >
+        <div className="star" />
+        <span> {noStars ? "Star Candidates" : `${starCount} Candidates`}</span>
+      </div>
       <div class="links">
         <a href="https://github.com/jasonlaster/emily">Help</a>
       </div>
     </div>
+  );
+}
+
+export default function renderCandidateBio({
+  selected,
+  starCount,
+  showStars,
+  toggleShowStars
+}) {
+  const candidate = candidates[selected];
+  if (!candidate) {
+    return (
+      <div className="candidate-bio">
+        <Banner
+          starCount={starCount}
+          toggleShowStars={toggleShowStars}
+          showStars={showStars}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div key={candidate.id} className="candidate-bio">
+      <Banner
+        starCount={starCount}
+        toggleShowStars={toggleShowStars}
+        showStars={showStars}
+      />
       <Header candidate={candidate} />
       <div className="candidate-description">
         <Article candidate={candidate} />
       </div>
- </div>
+    </div>
+  );
 }
